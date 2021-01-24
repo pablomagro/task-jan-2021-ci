401317fc217eca586773c43e33f7dedd3a5d0b32
Pablo Magro <pablom@traffic.net.nz>
2021-01-24 20:06:55 +1300

    Misc changes.

--- client/src/googleAuth.js
+++ client/src/googleAuth.js
@@ -5,7 +5,8 @@ const GoogleAuthContext = React.createContext()
 
 export const GoogleAuthProvider = ({ children }) => {
   const googleAuth = useGoogleLogin({
-    clientId: process.env.GOOGLE_CLIENT_ID, // Your clientID from Google.
+    // clientId: process.env.GOOGLE_CLIENT_ID, // Your clientID from Google.
+    clientId: '975037514405-j3muqadtcb3bnm2uq59pc9rs588u6e51.apps.googleusercontent.com', // Your clientID from Google.
   })
 
   return (
